388ffb47245c8f2c13e71cce8d728e4e73177a40
Scott Hoecker <hoeckers@gmail.com>
2022-04-18 21:17:18 -0500

    Init

---
+++ todo.rb
@@ -0,0 +1,60 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
+get "/" do
+  redirect "/lists"
+end
+
+# View all lists
+get "/lists" do
+  @lists = session[:lists]
+
+  erb :lists, layout: :layout
+end
+
+# Render the new list form
+get "/lists/new" do
+  erb :new_list, layout: :layout
+end
+
+# Return error message if the list name is invalid. Return nil if name is valid.
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    "The list name must be between 1 and 100 characters"
+  elsif session[:lists].any? { |list| list[:name] == name }
+    "The list name must be unique"
+  end
+end
+
+# Create a new list
+post '/lists' do
+  list_name = params[:list_name].strip
+  id = session[:lists].size.to_s
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << { id: id, name: list_name, todos: [] }
+    session[:success] = "The list has been created."
+    redirect "/lists"
+  end
+end
+
+get '/lists/:list_id' do
+  id = params[:list_id].to_i
+  @list = session[:lists][id]
+
+  erb :display_list, layout: :layout
+end
