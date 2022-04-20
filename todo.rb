c40a0f6ff0ba68311a02e26cc9bb36fe88e15b48
Scott Hoecker <hoeckers@gmail.com>
2022-04-20 13:55:49 -0500

    Work on marking todos completed or not

--- todo.rb
+++ todo.rb
@@ -53,6 +53,7 @@ post '/lists' do
   end
 end
 
+# View one specific list
 get '/lists/:list_id' do
   id = params[:list_id].to_i
   @list = session[:lists][id]
@@ -60,14 +61,74 @@ get '/lists/:list_id' do
   erb :list, layout: :layout
 end
 
-get 'lists/:list_id/edit' do
+# Render the list name change form
+get '/lists/:list_id/edit' do
   id = params[:list_id].to_i
   @list = session[:lists][id]
 
   erb :edit, layout: :layout
 end
 
-post 'lists/:list_id/edit' do
+# Update existing list name
+post '/lists/:list_id' do
   id = params[:list_id].to_i
   @list = session[:lists][id]
+  list_name = params[:list_name].strip
+
+  error = error_for_list_name(list_name)
+
+  if error
+    session[:error] = error
+    erb :edit, layout: :layout
+  else
+    @list[:name] = list_name
+    session[:success] = "The list name has been changed to \"#{list_name}\"."
+    redirect "/lists/#{id}"
+  end
+end
+
+# Delete list
+post '/lists/:list_id/destroy' do
+  id = params[:list_id].to_i
+  name = session[:lists][id][:name]
+  session[:lists].delete_at(id)
+  session[:success] = "The list \"#{name}\" was deleted"
+  redirect '/lists'
+end
+
+# Validate new to-do name
+def error_for_todo(list, name)
+  if !(1..100).cover? name.size
+    "The todo name must be between 1 and 100 characters"
+  elsif list[:todos].any? { |todo| todo[:name] == name }
+    "The todo name must be unique"
+  end
+end
+
+# Add to-do item to a list
+post '/lists/:list_id/todos' do
+  list_id = params[:list_id].to_i
+  @list = session[:lists][list_id]
+  text = params[:todo].strip
+
+  error = error_for_todo(@list, text)
+
+  if error
+    session[:error] = error
+    erb :list, layout: :layout
+  else
+    @list[:todos] << { name: params[:todo], completed: false }
+    session[:success] = "The todo '#{params[:todo]}' was added."
+    redirect "/lists/#{list_id}"
+  end
+end
+
+# Delete a specific to-do item from a list
+post '/lists/:list_id/todos/:todo_id/destroy' do
+  list_id = params[:list_id].to_i
+  todo_id = params[:todo_id].to_i
+  todo_name = session[:lists][list_id][:todos][todo_id][:name]
+  session[:lists][list_id][:todos].delete_at(todo_id)
+  session[:success] = "The todo item '#{todo_name}' was deleted"
+  redirect "/lists/#{list_id}"
 end
